48a217888d2fd2728957f0148fdebf804b67d686
Eduardo M KALINOWSKI <eduardo@kalinowski.com.br>
2018-08-01 15:31:27 -0300

    Added Makefile for Perl module.

--- perl/lib/KGtk3/ComboModel.pm
+++ perl/lib/KGtk3/ComboModel.pm
@@ -1,3 +1,9 @@
+package KGtk3::ComboModel;
+use strict;
+use warnings;
+
+our $VERSION = '1.0.0';
+
 use Glib::Object::Introspection;
 Glib::Object::Introspection->setup(
   basename => 'KGtk3ComboModel',
