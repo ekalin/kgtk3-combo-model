fb8c565fe7d51148b846f239cb946a95e0b670d0
Eduardo M KALINOWSKI <eduardo@kalinowski.com.br>
2018-08-03 13:42:01 -0300

    Implemented basic functionality of get_n_children.

--- src/kgtk3-combo-model.c
+++ src/kgtk3-combo-model.c
@@ -15,6 +15,9 @@ static void kgtk3_combo_model_tree_model_init(GtkTreeModelIface *iface);
 static GtkTreeModelFlags kgtk3_combo_model_get_flags(GtkTreeModel *model);
 static gint kgtk3_combo_model_get_n_columns(GtkTreeModel *model);
 static GType kgtk3_combo_model_get_column_type(GtkTreeModel *model, gint index);
+static gboolean kgtk3_combo_model_get_iter(GtkTreeModel *model, GtkTreeIter *iter, GtkTreePath *path);
+static gboolean kgtk3_combo_model_iter_next(GtkTreeModel *model, GtkTreeIter *iter);
+static gint kgtk3_combo_model_iter_n_children(GtkTreeModel *model, GtkTreeIter *iter);
 
 
 G_DEFINE_TYPE_WITH_CODE(KGtk3ComboModel, kgtk3_combo_model, G_TYPE_OBJECT,
@@ -43,15 +46,19 @@ kgtk3_combo_model_tree_model_init(GtkTreeModelIface *iface)
   iface->get_flags = kgtk3_combo_model_get_flags;
   iface->get_n_columns = kgtk3_combo_model_get_n_columns;
   iface->get_column_type = kgtk3_combo_model_get_column_type;
+  iface->get_iter = kgtk3_combo_model_get_iter;
   /*
-  iface->get_iter = gtk_tree_model_filter_get_iter;
   iface->get_path = gtk_tree_model_filter_get_path;
   iface->get_value = gtk_tree_model_filter_get_value;
-  iface->iter_next = gtk_tree_model_filter_iter_next;
+  */
+  iface->iter_next = kgtk3_combo_model_iter_next;
+  /*
   iface->iter_previous = gtk_tree_model_filter_iter_previous;
   iface->iter_children = gtk_tree_model_filter_iter_children;
   iface->iter_has_child = gtk_tree_model_filter_iter_has_child;
-  iface->iter_n_children = gtk_tree_model_filter_iter_n_children;
+  */
+  iface->iter_n_children = kgtk3_combo_model_iter_n_children;
+  /*
   iface->iter_nth_child = gtk_tree_model_filter_iter_nth_child;
   iface->iter_parent = gtk_tree_model_filter_iter_parent;
   iface->ref_node = gtk_tree_model_filter_ref_node;
@@ -104,5 +111,44 @@ kgtk3_combo_model_get_column_type(GtkTreeModel *model, gint index)
     return G_TYPE_BOOLEAN;
   } else {
     return gtk_tree_model_get_column_type(cmodel->base_model, index);
+  }
+}
+
+
+static
+gboolean
+kgtk3_combo_model_get_iter(GtkTreeModel *model, GtkTreeIter *iter, GtkTreePath *path)
+{
+  g_return_val_if_fail(KGTK3_IS_COMBO_MODEL(model), 0);
+
+  // TODO
+  return gtk_tree_model_get_iter(KGTK3_COMBO_MODEL(model)->base_model, iter, path);
+}
+
+
+static
+gboolean
+kgtk3_combo_model_iter_next(GtkTreeModel *model, GtkTreeIter *iter)
+{
+  g_return_val_if_fail(KGTK3_IS_COMBO_MODEL(model), 0);
+
+  // TODO
+  return gtk_tree_model_iter_next(KGTK3_COMBO_MODEL(model)->base_model, iter);
+}
+
+
+static
+gint
+kgtk3_combo_model_iter_n_children(GtkTreeModel *model, GtkTreeIter *iter)
+{
+  g_return_val_if_fail(KGTK3_IS_COMBO_MODEL(model), 0);
+
+  gint n_children = gtk_tree_model_iter_n_children(KGTK3_COMBO_MODEL(model)->base_model, iter);
+  // TODO
+
+  if (iter == NULL) {
+    return n_children;
+  } else {
+    return n_children > 0 ? n_children + 2 : 0;
   }
 }
--- test/test-combo-model.c
+++ test/test-combo-model.c
@@ -31,7 +31,42 @@ main(int argc, char *argv[])
 
   gtk_tree_store_append(model_store, &top_level, NULL);
   gtk_tree_store_set(model_store, &top_level,
-                     0, 1, 1, "Root 1", -1);
+                     0, 1,
+                     1, "Root 1", -1);
+
+  gtk_tree_store_append(model_store, &top_level, NULL);
+  gtk_tree_store_set(model_store, &top_level,
+                     0, 10,
+                     1, "Root 2", -1);
+  gtk_tree_store_append(model_store, &child, &top_level);
+  gtk_tree_store_set(model_store, &child,
+                     0, 11,
+                     1, "Child 2.1", -1);
+  gtk_tree_store_append(model_store, &child, &top_level);
+  gtk_tree_store_set(model_store, &child,
+                     0, 23,
+                     1, "Child 2.2", -1);
+  gtk_tree_store_append(model_store, &child, &top_level);
+  gtk_tree_store_set(model_store, &child,
+                     0, 30,
+                     1, "Child 2.3", -1);
+
+  gtk_tree_store_append(model_store, &top_level, NULL);
+  gtk_tree_store_set(model_store, &top_level,
+                     0, 5,
+                     1, "Root 3", -1);
+  gtk_tree_store_append(model_store, &child, &top_level);
+  gtk_tree_store_set(model_store, &child,
+                     0, 6,
+                     1, "Child 3.1", -1);
+  gtk_tree_store_append(model_store, &subchild, &child);
+  gtk_tree_store_set(model_store, &subchild,
+                     0, 16,
+                     1, "Child 3.1.1", -1);
+  gtk_tree_store_append(model_store, &child, &top_level);
+  gtk_tree_store_set(model_store, &child,
+                     0, 7,
+                     1, "Child 3.2", -1);
 
   GtkTreeModel *model = GTK_TREE_MODEL(model_store);
   KGtk3ComboModel *cmodel_k = kgtk3_combo_model_new(model);
@@ -70,6 +105,20 @@ main(int argc, char *argv[])
         "should return existing column type (column 1)");
   check(gtk_tree_model_get_column_type(cmodel, 2) == G_TYPE_BOOLEAN,
         "should return boolean type for last column");
+
+  /*
+   * Existence of children
+   */
+  check(gtk_tree_model_iter_n_children(cmodel, NULL) == 3,
+        "should return number of root children when NULL is passed to iter_n_children");
+
+  gtk_tree_model_get_iter_first(cmodel, &top_level);
+  check(gtk_tree_model_iter_n_children(cmodel, &top_level) == 0,
+        "should return 0 children if original has children - iter_n_children");
+
+  gtk_tree_model_iter_next(cmodel, &top_level);
+  check(gtk_tree_model_iter_n_children(cmodel, &top_level) == 5,
+        "should return two extra children if original has children - iter_n_children");
 
 
   /*
